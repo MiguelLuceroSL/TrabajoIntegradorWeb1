6231a9f025175f49fb23efdf8a87b7088cef342a
MiguelLuceroSL <theflixgame8@gmail.com>
2023-05-21 22:22:03 -0300

    Cambios del dia 21/05/2023
    Cambios por el momento son estos..

--- Asfalto Puntano/js/index.js
+++ Asfalto Puntano/js/index.js
@@ -113,3 +113,181 @@ function nextV(){
         break;
     }
 }
+
+//Sonia
+let intervaloS = setInterval(nextS, 3000);
+
+function detenerS(){
+    clearInterval(intervaloS);
+}
+
+document.querySelector(".prevS").addEventListener("click",detenerS);
+document.querySelector(".nextS").addEventListener("click",detenerS);
+
+function prevS(){
+    let imagen = document.getElementById("fotos6");
+    switch (imagen.className){
+        case "foto1":
+            imagen.src="../images/sonia4.jpg";
+            imagen.className = "foto4";
+        break;
+        case "foto2":
+            imagen.src="../images/sonia1.jpg";
+            imagen.className = "foto1";
+        break;
+        case "foto3":
+            imagen.src="../images/sonia2.jpg";
+            imagen.className = "foto2";
+        break;
+        case "foto4":
+            imagen.src="../images/sonia3.jpg";
+            imagen.className = "foto3";
+        break;
+    }
+}
+
+function nextS(){
+    let imagen = document.getElementById("fotos6");
+    switch (imagen.className){
+        case "foto1":
+            imagen.src="../images/sonia2.jpg";
+            imagen.className = "foto2";
+        break;
+        case "foto2":
+            imagen.src="../images/sonia3.jpg";
+            imagen.className = "foto3";
+        break;
+        case "foto3":
+            imagen.src="../images/sonia4.jpg";
+            imagen.className = "foto4";
+        break;
+        case "foto4":
+            imagen.src="../images/sonia1.jpg";
+            imagen.className = "foto1";
+        break;
+    }
+}
+
+//Dario
+let intervaloD = setInterval(nextD, 3000);
+
+function prevD(){
+    let imagen = document.getElementById("fotos7");
+    switch (imagen.className){
+        case "foto1":
+            imagen.src="../images/dario5.jpg";
+            imagen.className = "foto5";
+        break;
+        case "foto2":
+            imagen.src="../images/dario1.jpg";
+            imagen.className = "foto1";
+        break;
+        case "foto3":
+            imagen.src="../images/dario2.jpg";
+            imagen.className = "foto2";
+        break;
+        case "foto4":
+            imagen.src="../images/dario3.jpg";
+            imagen.className = "foto3";
+        break;
+        case "foto5":
+            imagen.src="../images/dario4.jpg";
+            imagen.className = "foto4";
+        break;
+    }
+}
+
+function detenerD(){
+    clearInterval(intervaloD);
+}
+
+document.querySelector(".prevD").addEventListener("click",detenerD);
+document.querySelector(".nextD").addEventListener("click",detenerD);
+
+function nextD(){
+    let imagen = document.getElementById("fotos7");
+    switch (imagen.className){
+        case "foto1":
+            imagen.src="../images/dario2.jpg";
+            imagen.className = "foto2";
+        break;
+        case "foto2":
+            imagen.src="../images/dario3.jpg";
+            imagen.className = "foto3";
+        break;
+        case "foto3":
+            imagen.src="../images/dario4.jpg";
+            imagen.className = "foto4";
+        break;
+        case "foto4":
+            imagen.src="../images/dario5.jpg";
+            imagen.className = "foto5";
+        break;
+        case "foto5":
+            imagen.src="../images/dario1.jpg";
+            imagen.className = "foto1";
+        break;
+    }
+}
+
+//Cuento
+let intervaloC = setInterval(nextC, 3000);
+
+function prevC(){
+    let imagen = document.getElementById("fotos8");//quede acaxd
+    switch (imagen.className){
+        case "foto1":
+            imagen.src="../images/dario5.jpg";
+            imagen.className = "foto5";
+        break;
+        case "foto2":
+            imagen.src="../images/dario1.jpg";
+            imagen.className = "foto1";
+        break;
+        case "foto3":
+            imagen.src="../images/dario2.jpg";
+            imagen.className = "foto2";
+        break;
+        case "foto4":
+            imagen.src="../images/dario3.jpg";
+            imagen.className = "foto3";
+        break;
+        case "foto5":
+            imagen.src="../images/dario4.jpg";
+            imagen.className = "foto4";
+        break;
+    }
+}
+
+function detenerD(){
+    clearInterval(intervaloD);
+}
+
+document.querySelector(".prevD").addEventListener("click",detenerD);
+document.querySelector(".nextD").addEventListener("click",detenerD);
+
+function nextD(){
+    let imagen = document.getElementById("fotos7");
+    switch (imagen.className){
+        case "foto1":
+            imagen.src="../images/dario2.jpg";
+            imagen.className = "foto2";
+        break;
+        case "foto2":
+            imagen.src="../images/dario3.jpg";
+            imagen.className = "foto3";
+        break;
+        case "foto3":
+            imagen.src="../images/dario4.jpg";
+            imagen.className = "foto4";
+        break;
+        case "foto4":
+            imagen.src="../images/dario5.jpg";
+            imagen.className = "foto5";
+        break;
+        case "foto5":
+            imagen.src="../images/dario1.jpg";
+            imagen.className = "foto1";
+        break;
+    }
+}
